1b9f1ae5706371d112b1f60fa61e9379981a7ae9
Saikat Rahman <saikatrahmanofficial@gmail.com>
2023-02-21 02:14:44 +0600

    changes

--- 1019.cs
+++ 1019.cs
@@ -2,23 +2,23 @@ namespace Beecrowd
 {
 	class Program
 	{
-		//static void Main(string[] args)
-		//{
-		//	//beecrowd | 1019
-		//	//Time Conversion
+		static void Main(string[] args)
+		{
+			//beecrowd | 1019
+			//Time Conversion
 
-		//	//Read an integer value, which is the duration in seconds
-		//	//of a certain event in a factory, and inform it expressed
-		//	//in hours:minutes:seconds.
+			//Read an integer value, which is the duration in seconds
+			//of a certain event in a factory, and inform it expressed
+			//in hours:minutes:seconds.
 
-		//	//The input file contains an integer N.
+			//The input file contains an integer N.
 
-		//	int time = Convert.ToInt32(Console.ReadLine());
-		//	int hour = time / 3600;
-		//	time = time - (hour * 3600);
-		//	int minutes = time / 60;
-		//	time = time - (minutes * 60);
-		//	Console.WriteLine($"{hour}:{minutes}:{time}");
-		//}
+			int time = Convert.ToInt32(Console.ReadLine());
+			int hour = time / 3600;
+			time = time - (hour * 3600);
+			int minutes = time / 60;
+			time = time - (minutes * 60);
+			Console.WriteLine($"{hour}:{minutes}:{time}");
+		}
 	}
 }
