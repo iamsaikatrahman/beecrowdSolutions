b278e8e662375092313b10e36db58a0b25d77ac3
Saikat Rahman <saikatrahmanofficial@gmail.com>
2023-02-21 02:11:18 +0600

    1019 solved

---
+++ 1019.cs
@@ -0,0 +1,24 @@
+namespace Beecrowd
+{
+	class Program
+	{
+		//static void Main(string[] args)
+		//{
+		//	//beecrowd | 1019
+		//	//Time Conversion
+
+		//	//Read an integer value, which is the duration in seconds
+		//	//of a certain event in a factory, and inform it expressed
+		//	//in hours:minutes:seconds.
+
+		//	//The input file contains an integer N.
+
+		//	int time = Convert.ToInt32(Console.ReadLine());
+		//	int hour = time / 3600;
+		//	time = time - (hour * 3600);
+		//	int minutes = time / 60;
+		//	time = time - (minutes * 60);
+		//	Console.WriteLine($"{hour}:{minutes}:{time}");
+		//}
+	}
+}
